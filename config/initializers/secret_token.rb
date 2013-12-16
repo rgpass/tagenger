78014f0f0c7fdb448e0969fca593f8304cf9a348
rgpass <rgpass@gmail.com>
2013-12-16 11:10:28 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tagenger::Application.config.secret_key_base = '3424a86df4f5e3dcfe715618c90760743ed3d402daeb9cde7ccba09229f8e6e8e469c44efd53426faea97086266260b1dabe9a729171135ef466fc3330d51be4'
