37a2d9876162a057cc05521ba8c6d37c56a504e0
Jordan <eightyfour@gmail.com>
2013-12-18 15:09:31 -0500

    adding rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe User do
+  it { should have_secure_password }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:tag) }
+end
