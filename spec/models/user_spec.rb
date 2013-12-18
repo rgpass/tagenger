52280aa994742c357b55ea16e1ad43ad4b8b94c4
Jordan <eightyfour@gmail.com>
2013-12-18 15:41:14 -0500

    commented out user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,4 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe User do
-  it { should have_secure_password }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password) }
-  it { should validate_presence_of(:tag) }
-end
+# describe User do
+# end
