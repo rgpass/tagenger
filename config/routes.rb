dd2828d42e07296f1ee26a00f783aae1b7412add
rgpass <rgpass@gmail.com>
2013-12-18 17:09:21 -0500

    Added notes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tagenger::Application.routes.draw do
   get "users/new"
   root to: 'users#new'
+  # root to: 'ui#index'   # This was causing a redirect loop on Heroku
 
   # This names it faq_path
   match '/signup',  to: 'users#new',        via: 'get'
