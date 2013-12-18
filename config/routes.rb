30604cc562a9d323dffbce4018dc51fceb08ae5f
rgpass <rgpass@gmail.com>
2013-12-18 15:17:58 -0500

    Added FAQ static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Tagenger::Application.routes.draw do
+  get "static_pages/faq"
   get 'ui(/:action)', controller: 'ui'
   root to: 'ui#index'
   # The priority is based upon order of creation: first created -> highest priority.
