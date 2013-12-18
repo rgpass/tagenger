4542bf561f5ca3c38f1bc2a145b1ddb3223a1502
rgpass <rgpass@gmail.com>
2013-12-18 14:56:23 -0500

    Set root to ui#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Tagenger::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  root to: 'ui#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
