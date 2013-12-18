4fc35ddd698ca6cca368d00a67f725ccc172f44d
rgpass <rgpass@gmail.com>
2013-12-18 16:20:55 -0500

    Experiment with routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Tagenger::Application.routes.draw do
   get "static_pages/faq"
-  get 'ui(/:action)', controller: 'ui'
+  # get 'ui(/:action)', controller: 'ui'
   root to: 'ui#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
