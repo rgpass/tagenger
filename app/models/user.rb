298bd4dda513c38b5d54737b655a9b08d138888e
rgpass <rgpass@gmail.com>
2013-12-19 10:18:05 -0500

    Add User tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	validates :first_name, presence: true, length: { maximum: 50 }
+	validates :last_initial, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	validates :tag, presence: true
+	has_secure_password
 end
