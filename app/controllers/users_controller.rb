11c011826b86ef0016d6a627cf0211aa77d20f3e
rgpass <rgpass@gmail.com>
2013-12-19 14:08:36 -0500

    Added authorization for Edit and Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -20,11 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -38,5 +39,16 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:first_name, :last_initial, :email, :tag, :password,
                                    :password_confirmation, :tndcheck)
+    end
+
+    # before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
