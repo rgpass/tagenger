4422130e4b8115d69ed350f0f3ea223d7f632eee
rgpass <rgpass@gmail.com>
2013-12-19 10:41:54 -0500

    Added secure password and related tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219144146) do
+ActiveRecord::Schema.define(version: 20131219151920) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -23,5 +23,7 @@ ActiveRecord::Schema.define(version: 20131219144146) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
