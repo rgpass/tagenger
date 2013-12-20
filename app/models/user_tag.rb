69eee7a77f0a812fa43cc8bc539d22911c5ff735
rgpass <rgpass@gmail.com>
2013-12-20 13:14:04 -0500

    Updated a couple user_tag tests

--- app/models/user_tag.rb
+++ app/models/user_tag.rb
@@ -1,4 +1,8 @@
 class UserTag < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :tag
+
+	# TODO: Uncomment after reverse relationship done
+	# validates :user_id, presence: true
+	# validates :tag_id, presence: true	
 end
