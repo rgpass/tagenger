9c4367b81e7ad34669534d4fb83736993a679691
rgpass <rgpass@gmail.com>
2013-12-20 16:49:38 -0500

    Initial Messages wiring

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,10 +2,9 @@ class Tag < ActiveRecord::Base
 	VALID_TAG_REGEX = /([A-Z]{2}[A-Z0-9]{3,10})/i
 	validates :tag_number, presence: true, format: { with: VALID_TAG_REGEX }, length: { maximum: 10 }
 
-	# has_many :user_tags, foreign_key: "user_id"#, class_name: "UserTag"
-	# has_many :users, through: :user_tags #, source: :users
-
 	has_many :user_tags
 	has_many :users, through: :user_tags
+
+	has_many :messages
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,10 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6}
 	validates :tndcheck, presence: true
 
-
-	# has_many :user_tags, foreign_key: "tag_id"
-	# has_many :tags, through: :user_tags #, source: :tags # TODO: Test to see if source is necessary
-
 	has_many :user_tags
 	has_many :tags, through: :user_tags
+
+	has_many :messages
 
 
 	def User.new_remember_token
