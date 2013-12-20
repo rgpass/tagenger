b86b503a7cb88648bea7237e721540dcb5d78eec
rgpass <rgpass@gmail.com>
2013-12-20 16:28:48 -0500

    Messed around with relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,11 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6}
 	validates :tndcheck, presence: true
 
-	has_many :user_tags, foreign_key: "tag_id"
-	has_many :tags, through: :user_tags, source: :tags
+	# has_many :user_tags, foreign_key: "tag_id"
+	# has_many :tags, through: :user_tags #, source: :tags # TODO: Test to see if source is necessary
+
+	has_many :user_tags
+	has_many :tags, through: :user_tags
 
 	def User.new_remember_token
     SecureRandom.urlsafe_base64
