1eca21bc838d226b4dbe728bbbb76467758b7d63
rgpass <rgpass@gmail.com>
2013-12-20 12:54:42 -0500

    May have fixed user_tag relationship

--- spec/models/user_tag_spec.rb
+++ spec/models/user_tag_spec.rb
@@ -4,7 +4,7 @@ describe UserTag do
   let(:user) { FactoryGirl.create(:user) }
   let(:tag)  { FactoryGirl.create(:tag) }
 
-  let(:user_tag) { user.user_tag.build(tag_id: tag.id) }
+  let(:user_tag) { user.user_tags.build(tag_id: tag.id) }
 
   subject { user_tag }
 
