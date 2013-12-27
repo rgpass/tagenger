537ef99f75550c4ab8a936fd11c9d60f1771f559
rgpass <rgpass@gmail.com>
2013-12-27 12:55:21 -0500

    Edited pull down format

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,10 @@
 //= require turbolinks
 //= require twitter/bootstrap
 //= require_tree .
+
+$(document).ready(function() {
+	$(".dropdown-menu li a").click(function(){
+	  var selText = $(this).text();
+	  $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+	});
+});
