12b64adba0e89548ea8e1b979284ff95e8ea6a17
rgpass <rgpass@gmail.com>
2013-12-29 15:53:56 -0500

    Trying to validate blank State in Submit

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :tag
+
+	attr_accessor :tag_state
+	validates :tag_number, presence: true
+	validates :content, presence: true
+	# validates :tag_state, presence: true
 end
